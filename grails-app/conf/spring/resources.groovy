171598530b5d2b4939ab29878a549539873945f2
ValenteFlores <flores.valente@gmail.com>
2021-08-31 20:45:48 -0500

    correcciones a Language en:US

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -2,4 +2,8 @@ import prod.cuernasoft.com.seguridad.UsuarioPasswordEncoderListener
 // Place your Spring DSL code here
 beans = {
     usuarioPasswordEncoderListener(UsuarioPasswordEncoderListener)
+    localeResolver(org.springframework.web.servlet.i18n.SessionLocaleResolver) {
+        defaultLocale = new Locale("en","US")
+        java.util.Locale.setDefault(defaultLocale)
+    }
 }
